b7c2d33f52ee6fb4df5c73dd840b5a39ae6f099c
imryanide <rayanrafisheikh@gmail.com>
2022-01-17 09:59:49 +0530

    Initial setup

---
+++ main.py
@@ -0,0 +1,38 @@
+
+import disnake
+from disnake.ext import commands
+from dotenv import load_dotenv
+import os
+
+
+#set bot token
+load_dotenv('.env')
+TOKEN = os.getenv('TOKEN')
+
+description = """An recon bot."""
+
+intents = disnake.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="?", description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+    game = disnake.Game("with nzu balls")
+    await bot.change_presence(activity=game)
+
+    print(f"Connected to client {bot.user}") 
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+
+
+bot.run(TOKEN)
